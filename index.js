b35e584bb4ed4c62fb66bd1430e4eceb3e063d66
Metsuge <51269699+Metsuge@users.noreply.github.com>
2020-05-05 12:02:01 +0300

    updates

--- index.js
+++ index.js
@@ -1,20 +1,45 @@
 const cards = document.querySelectorAll('.memory-card');
 
 let hasFlippedCard = false;
+let lockBoard = false;
 let firstCard, secondCard;
 
 function flipCard(){
-    this.classList.add('flip');
+  if (lockBoard) return; //if true, all stops, and the following code won't be executed 
+  this.classList.add('flip');
 
   if(!hasFlippedCard){
       //this is first click
     hasFlippedCard = true;
     firstCard=this;
-  }else{
-      //this is second click
-      hasFlippedCard = false;
-      secondCard = this;
-  }
+
+    return;
+  } 
+    //this is second click
+    hasFlippedCard = false;
+    secondCard = this;
+    checkForMatch();
 }
+
+function checkForMatch(){
+  let isMatch = firstCard.dataset.framework === secondCard.dataset.framework;
+
+  isMatch ? disableCards() : unflipCards();
+}
+
+function disableCards(){
+  firstCard.removeEventListener('click', flipCard);
+  secondCard.removeEventListener('click', flipCard);
+}
+
+function unflipCards() {
+  lockBoard = true;
+  setTimeout(() => {
+    firstCard.classList.remove('flip');
+    secondCard.classList.remove('flip');
+  }, 1000);
+  lockBoard = false;
+}
+
 
 cards.forEach(card => card.addEventListener('click', flipCard));
