0bd6c49312dd44a1f521215d960b2945779ef73a
Metsuge <51269699+Metsuge@users.noreply.github.com>
2020-05-10 16:53:38 +0300

    updates on flipping time

--- index.js
+++ index.js
@@ -15,14 +15,10 @@
     memoryCard.appendChild(cardImg).src = './images/aurelia.svg';
     memoryCard.appendChild(backImg).src = './images/js-badge.svg';
 
+    //creating random nr for the id of seach card
+    //so that you can compare datasets and see if the players cards are the same as the given sequence 
     let idOfCard = Math.floor(Math.random()*500);
-
     memoryCard.setAttribute('data-framework', idOfCard)
-
-
-    
-
-    
 
     document.body.appendChild(newCard);
     newCard.appendChild(memoryCard);
@@ -31,9 +27,7 @@
 })(8)
 
 const cards = [...document.querySelectorAll(".memory-card")];
-
 const button = document.querySelector('.play');
-
 const messagePlay = document.getElementById('score');
 const level3 = document.getElementsByClassName('leve3');
 
@@ -44,6 +38,8 @@ let score = 0;
 let answer = false;
 let speedOfFlip = 400;
 let cardNumber = 3;
+let nrOfCardsFlipped = 0;
+
 
 function makeRandomList(){ //nera pasikartojanciu kortu
   while (randomListOfCards.length < cardNumber) {
@@ -55,51 +51,53 @@ function makeRandomList(){ //nera pasikartojanciu kortu
   };  
 };
 
+
+
+
 function flipCardsAuto(i){//press Play, random sequence is flipping
   let randomCard = randomListOfCards[i];
-  
-
   randomCard.classList.add('flip') 
+
   if (i<randomListOfCards.length){
+
     setTimeout(function(){
       i++; 
       flipCardsAuto(i);
+     
     }, speedOfFlip); 
+     nrOfCardsFlipped++;
   };
-    setTimeout(() => {
+  
+  console.log(nrOfCardsFlipped);
+  
+  if(nrOfCardsFlipped === randomListOfCards.length){
+    setTimeout(function(){
 
-      cards.forEach(card =>{
-      card.classList.remove('flip')
-
-      }); 
+      randomListOfCards.forEach(card => {
+      card.classList.remove('flip') })
       lockBoard = false;
-    }, 1700)
-
-
-  
-  
-};
+      nrOfCardsFlipped = 0;
+   
+    }, 1000);
+  }
  
+};
+
+
+
 function play(){
   makeRandomList();
   flipCardsAuto(0);
 };
 
-// (function shuffle(){
-//   cards.forEach(card => {
-//     let randomPos = Math.floor(Math.random()*cards.length);
-//     card.style.order = randomPos;
 
-//   });
-// })();
 
 function flipCard(){ //zaidejas flippina ir kai pasirenka 3 kortas is kart tikrina
   if(lockBoard) return; //jei lenta uzrakinta, nk negali spaust
-  
-  this.classList.add('flip'); 
+  this.classList.add('flip');
+
   setTimeout(()=>{
     this.classList.remove('flip');
-    
   }, speedOfFlip);
 
   let playerCard = this;
