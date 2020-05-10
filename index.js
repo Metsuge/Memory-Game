3abfa89e96b3afce2de2b75c43250b7ad427c8f8
Metsuge <51269699+Metsuge@users.noreply.github.com>
2020-05-10 16:24:23 +0300

    created DOM elements with js

--- index.js
+++ index.js
@@ -1,16 +1,48 @@
-const cards = document.querySelectorAll('.memory-card');
+
+(function createDOMElements(i){//i = how many cards on screen
+  let newCard = document.createElement('section')
+  for(i=0;i<8;i++){
+    
+    let memoryCard = document.createElement('div');
+    let cardImg = document.createElement('img');
+    let backImg = document.createElement('img');
+
+    newCard.className = "memory-game"
+    memoryCard.className = "memory-card";
+    cardImg.className = "front-face";
+    backImg.className = "back-face";
+    
+    memoryCard.appendChild(cardImg).src = './images/aurelia.svg';
+    memoryCard.appendChild(backImg).src = './images/js-badge.svg';
+
+    let idOfCard = Math.floor(Math.random()*500);
+
+    memoryCard.setAttribute('data-framework', idOfCard)
+
+
+    
+
+    
+
+    document.body.appendChild(newCard);
+    newCard.appendChild(memoryCard);
+  }
+  
+})(8)
+
+const cards = [...document.querySelectorAll(".memory-card")];
+
 const button = document.querySelector('.play');
 
 const messagePlay = document.getElementById('score');
-
-let lockBoard = true;
+const level3 = document.getElementsByClassName('leve3');
 
 let randomListOfCards = [];
 let playerListOfCards = [];
-
+let lockBoard = true;
 let score = 0;
 let answer = false;
-
+let speedOfFlip = 400;
 let cardNumber = 3;
 
 function makeRandomList(){ //nera pasikartojanciu kortu
@@ -19,40 +51,47 @@ function makeRandomList(){ //nera pasikartojanciu kortu
     let randomCard = cards[randomNr];
     if(!randomListOfCards.includes(randomCard)){
       randomListOfCards.push(randomCard); 
-    } 
-  }
-}
+    }; 
+  };  
+};
 
 function flipCardsAuto(i){//press Play, random sequence is flipping
-
-  let randomCard = randomListOfCards[i]
- 
+  let randomCard = randomListOfCards[i];
+  
 
   randomCard.classList.add('flip') 
   if (i<randomListOfCards.length){
     setTimeout(function(){
       i++; 
       flipCardsAuto(i);
-    }, 500); 
+    }, speedOfFlip); 
+  };
     setTimeout(() => {
-    randomCard.classList.remove('flip')
-    }, 1500); 
-  }
-  lockBoard = false;
-} 
+
+      cards.forEach(card =>{
+      card.classList.remove('flip')
+
+      }); 
+      lockBoard = false;
+    }, 1700)
+
+
+  
+  
+};
  
 function play(){
   makeRandomList();
   flipCardsAuto(0);
 };
 
-(function shuffle(){
-  cards.forEach(card => {
-    let randomPos = Math.floor(Math.random()*cards.length);
-    card.style.order = randomPos;
+// (function shuffle(){
+//   cards.forEach(card => {
+//     let randomPos = Math.floor(Math.random()*cards.length);
+//     card.style.order = randomPos;
 
-  });
-})();
+//   });
+// })();
 
 function flipCard(){ //zaidejas flippina ir kai pasirenka 3 kortas is kart tikrina
   if(lockBoard) return; //jei lenta uzrakinta, nk negali spaust
@@ -61,33 +100,33 @@ function flipCard(){ //zaidejas flippina ir kai pasirenka 3 kortas is kart tikri
   setTimeout(()=>{
     this.classList.remove('flip');
     
-  }, 1000)
+  }, speedOfFlip);
 
   let playerCard = this;
   playerListOfCards.push(playerCard);
 
   if(playerListOfCards.length === randomListOfCards.length){
     checkAnswer();
-  } 
-}
+  };
+};
 
 function checkAnswer() {  
-  let correctNrOfCards = 0
+  let correctNrOfCards = 0;
   for (i=0;i<randomListOfCards.length;i++){
     let pcCard = randomListOfCards[i];
-    let gamerCard = playerListOfCards[i]
+    let gamerCard = playerListOfCards[i];
     if(pcCard.dataset.framework === gamerCard.dataset.framework){
       correctNrOfCards++;
     }; 
   };
     
-      if(correctNrOfCards === randomListOfCards.length){
-        answer = true;
-        correctNrOfCards = 0;
-      } else {
-        answer = false;
-        correctNrOfCards = 0;
-      }
+  if(correctNrOfCards === randomListOfCards.length){
+    answer = true;
+    correctNrOfCards = 0;
+  } else {
+    answer = false;
+    correctNrOfCards = 0;
+  };
 
   if(answer){
     score++;
@@ -96,20 +135,37 @@ function checkAnswer() {
     if(score>0){
       score--;      
     }
-  }
+  };
+
   document.getElementById("score").innerHTML = "Score: " + score;
   randomListOfCards=[];
   playerListOfCards = [];
-  levelUp()
+  levelUp();
 }
 
 function levelUp(){
-  if(score === 5){
-    cardNumber++
+  let currentScore = score;
+  if(score === 3){
+    cardNumber++;
+    speedOfFlip = speedOfFlip-50;
+    document.getElementById("level").innerHTML = "Level 2!";
+    currentScore++;
+  } else if(score === 5){
+    cardNumber++;
+    speedOfFlip = speedOfFlip+50;
+    level3.style.visibility = 'visible';
   }
-}
+
+};
 
 
 button.addEventListener('click', play);
 cards.forEach(card => card.addEventListener('click', flipCard));
+
+
+
+
+
+
+
 
